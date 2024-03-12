57e55b339b2bf2effa5e35f243aac97a9791046c
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-03-12 09:42:27 +0000

    API fix

--- src/Functions/apiFunctions.ts
+++ src/Functions/apiFunctions.ts
@@ -9,7 +9,7 @@ export async function fetchLocationTemp(url: string, lon: string, lat: string) {
       lat: lat,
     },
     headers: {
-      "X-RapidAPI-Key": "46998e23d5msh038d3313907099dp1985d5jsn2eae7b9ddc7c",
+      "X-RapidAPI-Key": "API-KEY-HERE",
       "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
     },
   }
